df4fafb3d671c2ec13b1eae370755fcd078c578c
Nikita Zorin <32186144+Fr0ggyz@users.noreply.github.com>
2017-09-28 22:57:21 +0300

    Update classwork.cpp

--- classwork.cpp
+++ classwork.cpp
@@ -4,17 +4,17 @@
 
 int main()
 {
-	std::cout << "Pi=" << 3.14 << std::endl;  // print Pi
-	/// 
+	std::cout << "Pi=" << 3.14 << std::endl;
+	///
 	/// 2
 	char c = ' ';
-	std::cout << 1 << c << 13 << 49 << std::endl;  // print 1 13 49
-	/// 
+	std::cout << 1 << c << 13 << 49 << std::endl;
+	///
 	/// 3
 	char b;
 	std::cin >> b;
-	std::cout << 1 << b << 13 << b << 49 << std::endl;  // print 1 13 49 with symbol between
-	/// 
+	std::cout << 1 << b << 13 << b << 49 << std::endl;
+	///
 	/// 4
 	int a1, a2, a3;
 	std::cout << " Write 3 numbers " << std::endl;
@@ -22,7 +22,7 @@ int main()
 	std::cin >> a2;
 	std::cin >> a3;
 	std::string f = "  ";
-	std::cout << a1 << f << a2 << f << a3 << std::endl;  // print 3 numbers with 2 spaces
+	std::cout << a1 << f << a2 << f << a3 << std::endl;
 	return 0;
 }
 
